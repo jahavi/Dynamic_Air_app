7b10b1bfd28988860a18e346a3483248469550a8
Sunki Reddy Jahnavi <98159788+jahavi@users.noreply.github.com>
2025-09-15 12:16:30 -0500

    env update

--- src/app/auth-service/firebase/firebase-config.ts
+++ src/app/auth-service/firebase/firebase-config.ts
@@ -1,9 +1,13 @@
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyDJqdrXQ8Zxp-OpUe3HGYN3984cBFAjbI4",
-  authDomain: "dynamic-air-42a4c.firebaseapp.com",
-  projectId: "dynamic-air-42a4c",
-  storageBucket: "dynamic-air-42a4c.firebasestorage.app",
-  messagingSenderId: "278323809825",
-  appId: "1:278323809825:web:ac6b95e8c753c1140b1d60"
-};
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDJqdrXQ8Zxp-OpUe3HGYN3984cBFAjbI4",
+//   authDomain: "dynamic-air-42a4c.firebaseapp.com",
+//   projectId: "dynamic-air-42a4c",
+//   storageBucket: "dynamic-air-42a4c.firebasestorage.app",
+//   messagingSenderId: "278323809825",
+//   appId: "1:278323809825:web:ac6b95e8c753c1140b1d60"
+// };
+
+import { environment } from '../../../environments/environment';
+
+export const firebaseConfig = environment.firebase;
--- src/app/auth-service/http/http-interceptor.ts
+++ src/app/auth-service/http/http-interceptor.ts
@@ -1,11 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { FirebaseAuth } from '../firebase/firebase-auth';
 import { inject } from '@angular/core';
+// import { environment } from '';
+import { environment } from '../../../environments/environment';
+
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(FirebaseAuth);
-  const authToken = "WW91IG11c3QgYmUgdGhlIGN1cmlvdXMgdHlwZS4gIEJyaW5nIHRoaXMgdXAgYXQgdGhlIGludGVydmlldyBmb3IgYm9udXMgcG9pbnRzICEh";
+  // const authToken = "WW91IG11c3QgYmUgdGhlIGN1cmlvdXMgdHlwZS4gIEJyaW5nIHRoaXMgdXAgYXQgdGhlIGludGVydmlldyBmb3IgYm9udXMgcG9pbnRzICEh";  
+  const authToken = environment.httpPostToken;
 
   // Clone the request to add the new header.
   const authReq = req.clone({
